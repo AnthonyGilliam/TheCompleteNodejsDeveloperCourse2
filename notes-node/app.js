d62e545849e11cc510fa643153a197c8b6aaf8c9
Anthony <anthony.gilliam@gmail.com>
2018-02-02 17:11:59 -0500

    Section 3:10

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,3 +1,13 @@
-console.log('starting app.');
+console.log('starting app');
 
-const fs = require()
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes');
+
+var res = notes.add(9, -2);
+
+console.log(res);
+
+// let user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, err => { if(err) console.log(err) });

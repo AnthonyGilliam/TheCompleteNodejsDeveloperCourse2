1056893705a9b886ec64fe778887d973b08991be
Anthony <anthony.gilliam@gmail.com>
2018-03-18 06:24:56 -0400

    Section 3:11

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,16 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-var res = notes.add(9, -2);
+let res = notes.add(9, -2);
 
-console.log(res);
+console.log(`true ${_.isString(true) ? 'is' : 'is not'} a string`);
+console.log(`'Andrew' ${_.isString('Andrew') ? 'is' : 'is not'} a string`);
+
+let filteredArray = _.uniq(['Anthony', 1, 'Anthony', 2, 3, 4]);
+console.log(`Filtered array:  ${filteredArray}`);
 
 // let user = os.userInfo();
 //

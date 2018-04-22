420035cf03a08472baf515a4f4db8ddf50c1c9d9
Anthony <anthony.gilliam@gmail.com>
2018-04-22 08:12:05 -0400

    5:45 - Express Middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const fs = require('fs');
 const handlebars = require('hbs');
 
 const app = express();
 
+app.use((req, res, next) =>{
+    let now = new Date().toString();
+    let log = `${now}:  ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n'
+        , err => { if (err) console.log('Unable to append to server.log') });
+    next();
+});
+
+//Un-comment when the site is under maintenance to block all requests
+// app.use((req, res, next) =>{
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'hbs');
 handlebars.registerPartials(__dirname + '/views/partials');
 handlebars.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -10,8 +29,6 @@ handlebars.registerHelper('getCurrentYear', () => {
 handlebars.registerHelper('screamIt', (text1, text2) => {
     return `${text1.toUpperCase()} ${text2.toUpperCase()}`;
 });
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express!!!</h1>');

d1532d61090a2aea3b48b78330416df5731031e6
Anthony <anthony.gilliam@gmail.com>
2020-01-23 19:00:09 -0500

    11:86 REST APIs and Mongoose (Task App) - Data Validatio and Sanitization: Part II

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,3 +1,4 @@
+const validator = require('validator');
 const mongoose = require('mongoose');
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 const databaseName = 'task-manager';
@@ -5,19 +6,72 @@ const databaseName = 'task-manager';
 async function main() {
     try {
         const User = mongoose.model('User', {
-            name: { type: String },
-            age: { type: Number }
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            email: {
+                type: String,
+                required: true,
+                trim: true,
+                lowercase: true,
+                validate(value) {
+                    if (!validator.isEmail(value)) {
+                        throw new Error('Email is invalid');
+                    }
+                }
+            },
+            password: {
+                type: String,
+                required: true,
+                minlength: 7,
+                trim: true,
+                validate(value) {
+                    if (value.toLowerCase() === 'password') {
+                        throw new Error('Password CANNOT be "password"');
+                    }
+                }
+            },
+            age: {
+                type: Number,
+                default: 0,
+                min: 0
+            }
+        });
+
+        const Task = mongoose.model('Task', {
+            description: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            completed: {
+                type: Boolean,
+                default: false
+            }
         });
 
         mongoose.connect(connectionURL
             , { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
         );
 
-        const me = new User({ name: 'Cheska', age: null });
+        const user = new User({
+            name: 'Anthony',
+            email: 'ag@gil.com',
+            password: 'Password'
+        });
 
-        let savedMe = await me.save();
+        const task = new Task({
+            description: '  Eat lunch'
+        });
 
-        console.log(savedMe);
+        // let savedUser = await user.save();
+
+        let savedTask = await task.save();
+
+        // console.log('User Saved:\n', savedUser);
+        console.log('Task Saved:\n', savedTask);
     } catch (err) {
         console.error(err);
     }
